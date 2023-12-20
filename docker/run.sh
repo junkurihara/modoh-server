7aa10098b6f46d03b1764e2a68a451537428a51f
Jun Kurihara <junkurihara@users.noreply.github.com>
2023-12-20 15:54:40 +0900

    update dockerfile to build with otel

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 CONFIG_FILE=/etc/modoh-server.toml
+DEFAULT_OTEL_ENDPOINT=http://localhost:4317
 
 # debug level logging
 if [ -z $LOG_LEVEL ]; then
@@ -18,8 +19,29 @@ else
   fi
 fi
 
-if  $WATCH ; then
-  RUST_LOG=${LOG_LEVEL} /modoh/bin/modoh-server --config ${CONFIG_FILE} -w
+# otel
+OTEL_ARG=""
+if [ -z $OTEL_ENDPOINT ]; then
+  OTEL_ENDPOINT=${DEFAULT_OTEL_ENDPOINT}
+fi
+if [ -z $DISABLE_OTEL ]; then
+  DISABLE_OTEL=false
 else
-  RUST_LOG=${LOG_LEVEL} /modoh/bin/modoh-server --config ${CONFIG_FILE}
+  if [ "$DISABLE_OTEL" = "true" ]; then
+    DISABLE_OTEL=true
+  else
+    DISABLE_OTEL=false
+  fi
+fi
+if $DISABLE_OTEL ; then
+  echo "modoh-server: OpenTelemetry disabled"
+else
+  echo "modoh-server: OpenTelemetry enabled with endpoint ${OTEL_ENDPOINT}"
+  OTEL_ARG="--otel --otlp-endpoint ${OTEL_ENDPOINT}"
+fi
+
+if  $WATCH ; then
+  RUST_LOG=${LOG_LEVEL} /modoh/bin/modoh-server --config ${CONFIG_FILE} -w ${OTEL_ARG}
+else
+  RUST_LOG=${LOG_LEVEL} /modoh/bin/modoh-server --config ${CONFIG_FILE} ${OTEL_ARG}
 fi
